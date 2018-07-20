42999d60227c1adf47cf22c12536f6ca9416588e
Antonia Siu <Antonia.Siu@softwire.com>
2018-07-20 17:19:30 +0100

    leech component

---
+++ src/leech.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class Leech extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        leeches: 0
+      };
+
+      this.getLeech = this.getLeech.bind(this);
+    }
+
+    getLeech () {
+        this.setState({
+          leeches: this.state.leeches + 1
+        });
+    }
+
+    render() {
+      return (
+        <div className="leeches">
+        <p>You have {this.state.leeches} leeches.</p>
+          <button onClick={this.getLeech}>get leech</button>
+        </div>
+      );
+    }
+  }
+
+export default Leech;
