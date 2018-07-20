edecf431d55cfab924abb6f944af4400b83d0813
eric-tong <erictong.designs@gmail.com>
2018-07-20 17:29:41 +0100

    Add fryer button

---
+++ src/leech-deepfryer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class LeechDeepfryer extends Component {
+    render() {
+        return (
+            <buttton className="Leech-Deep-Fryer">
+                DEEP FRY
+            </buttton>
+        );
+    }
+}
+
+export default LeechDeepfryer;
