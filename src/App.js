ea13c84bdf83b75a1ac33e2c8b0bb7fb45dd7410
Robin Leach <Robin.Leach@softwire.com>
2018-07-23 09:29:08 +0100

    Added my v v basic component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const element = <h1>Hello, world!</h1>;
     return (
       <div className="App">
         <header className="App-header">
@@ -13,9 +14,15 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <RobinsElement />
       </div>
     );
   }
 }
+
+function RobinsElement() {
+  return <h1>Hello, this will be Robin's element of fun and glory</h1>;
+}
+
 
 export default App;
